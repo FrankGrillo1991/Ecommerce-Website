0f2753722f9d40340dc1eee6b0faaf4b26212c3a
FrankGrillo1991 <fsg12491@aol.com>
2025-01-22 05:48:06 -0500

    Update ecommerce.js

--- ecommerce.js
+++ ecommerce.js
@@ -2,9 +2,9 @@ const close = document.querySelector(".close");
 const open = document.querySelector(".ham");
 const menu = document.querySelector(".menu");
 close.addEventListener("click", () =>  {
-    menu.visibility.style = "hidden";
+    menu.style.visibility = "hidden";
 });
 open.addEventListener("click", () => {
-    menu.visibility.style = "visible";
+    menu.style.visibility = "visible";
 });
 
