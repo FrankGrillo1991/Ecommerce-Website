24ec970767f0868be723ed5f0b669b74d0e70690
FrankGrillo1991 <fsg12491@aol.com>
2025-06-29 06:10:05 -0400

    Update ecommerce.js

--- ecommerce.js
+++ ecommerce.js
@@ -8,3 +8,25 @@ open.addEventListener("click", () => {
     menu.style.visibility = "visible";
 });
 
+const filterButtons = document.querySelectorAll(".filter-btn");
+const productCards = document.querySelectorAll(".product-card");
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        document.querySelector(".filter-btn.active").classList.remove("active");
+        button.classList.add("active");
+
+        const category = button.getAttribute("data-category");
+
+        productCards.forEach(card => {
+
+            const cardCategory = card.getAttribute("data-category");
+
+            if (category === "all" || card.getAttribute("data-category") === category) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    });
+});
